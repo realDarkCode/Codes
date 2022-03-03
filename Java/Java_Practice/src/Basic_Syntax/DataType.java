c0eca0e864933ce9aa5f6564b72c006737c8bb4d
Tanzil Islam <tanzil.ncpsc@gmail.com>
2022-03-03 23:40:06 +0600

    java.DataType

--- Java/Java_Practice/src/Basic_Syntax/DataType.java
+++ Java/Java_Practice/src/Basic_Syntax/DataType.java
@@ -2,6 +2,16 @@ package Basic_Syntax;
 
 public class DataType {
     public static void main(String[] args) {
-        
+        /* 
+            2(8) /2  
+            2 to the power 8 = 256 
+            / 2
+            -128  - (128 - 1)
+        */
+        int number1 = 50;
+        int number2 = 65;
+        int number3 = 87;
+        int result = number1 + number2 + number3;
+        System.out.println(result);
     }
 }
