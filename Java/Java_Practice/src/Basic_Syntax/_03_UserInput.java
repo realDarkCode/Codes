bb523422a3498f23f629ff9bcb25bf3d1c470320
Tanzil Islam <tanzil.ncpsc@gmail.com>
2022-03-04 11:48:12 +0600

    reouse leak issue solved

--- Java/Java_Practice/src/Basic_Syntax/_03_UserInput.java
+++ Java/Java_Practice/src/Basic_Syntax/_03_UserInput.java
@@ -1,12 +1,14 @@
 package Basic_Syntax;
+
 import java.util.Scanner;
 
 public class _03_UserInput {
     public static void main(String[] args) {
-        // int number; 
+        // int number;
         Scanner input = new Scanner(System.in);
         // int number = input.nextInt();
         boolean hasInteger = input.hasNextInt();
+        input.close();
         System.out.println(hasInteger);
     }
 }
