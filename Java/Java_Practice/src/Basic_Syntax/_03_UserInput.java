3bd89fde84025cfd86fa1c5444b0f4b74de04658
Tanzil Islam <tanzil.ncpsc@gmail.com>
2022-03-04 11:19:35 +0600

    User Input from keyboard

--- Java/Java_Practice/src/Basic_Syntax/_03_UserInput.java
+++ Java/Java_Practice/src/Basic_Syntax/_03_UserInput.java
@@ -1,7 +1,12 @@
 package Basic_Syntax;
+import java.util.Scanner;
 
 public class _03_UserInput {
     public static void main(String[] args) {
-        System.out.println();
+        // int number; 
+        Scanner input = new Scanner(System.in);
+        // int number = input.nextInt();
+        boolean hasInteger = input.hasNextInt();
+        System.out.println(hasInteger);
     }
 }
