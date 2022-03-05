5ec29aff2f9178a5cdb8e3995d92e3b358da994f
Tanzil Islam <tanzil.ncpsc@gmail.com>
2022-03-05 16:17:12 +0600

    string practice

--- Java/Java_Practice/src/Basic_Syntax/_04_String.java
+++ Java/Java_Practice/src/Basic_Syntax/_04_String.java
@@ -2,11 +2,11 @@ package Basic_Syntax;
 
 public class _04_String {
     public static void main(String[] args) {
-        String text = "Hello Hello";
+        // String text = "Hello Hello";
         // System.out.println(text.substring(0, 5));
         // System.out.println(text.replace("l", "gg"));
         // System.out.println(text.startsWith("Hel"));
         // System.out.println(text[0]);
-        
+
     }
 }
