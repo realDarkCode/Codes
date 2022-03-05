c879fbcdb71ec0dc725870d5f05bfef5e7b10344
Tanzil Islam <tanzil.ncpsc@gmail.com>
2022-03-05 16:16:51 +0600

    string practice

---
+++ Java/Java_Practice/src/Basic_Syntax/_04_String.java
@@ -0,0 +1,12 @@
+package Basic_Syntax;
+
+public class _04_String {
+    public static void main(String[] args) {
+        String text = "Hello Hello";
+        // System.out.println(text.substring(0, 5));
+        // System.out.println(text.replace("l", "gg"));
+        // System.out.println(text.startsWith("Hel"));
+        // System.out.println(text[0]);
+        
+    }
+}
