43cd014c19eea2213d2552ad8864035e92a83ef0
Tanzil Islam <tanzil.ncpsc@gmail.com>
2022-03-13 18:44:58 +0600

    practice set 10 inheritance skipped

---
+++ Java/Java_Practice/src/CodeWithHarry_Exercise/Practice_set_10_Inheritance.java
@@ -0,0 +1,63 @@
+package CodeWithHarry_Exercise;
+
+/**
+ * Fully younked from: codewithHarry Website:
+ * link: https://codewithharry.com/videos/java-tutorials-for-beginners-52
+ */
+
+// ? 1. Create a class circle and use inheritance to create another class
+// cylinder from it
+// ? 2. Create a class rectangle and use inheritance to create another class
+// cuboid, try to keep it as close to the real-world scenario as possible
+// ? 3. Create a method for area and volume in 1
+// ? 4. create methods for area & volume in 2 also create getters and setters
+// ? 5. What is the order of constructor execution for the following inheritance
+// hierarchy
+/**
+ * Base
+ * 
+ * Derived 1
+ * 
+ * Derived 2
+ * 
+ * Derived obj = new Derived 2( );
+ * Which constructor(s) will be executed & in what order?
+ */
+class Circle {
+    public int radius;
+
+    Circle() {
+        System.out.println("I am non param of circle");
+    }
+
+    Circle(int r) {
+        System.out.println("I am circle parameterized constructor");
+        this.radius = r;
+    }
+
+    public double area() {
+        return Math.PI * this.radius * this.radius;
+    }
+}
+
+class Cylinder1 extends Circle {
+    public int height;
+
+    Cylinder1(int r, int h) {
+        super(r);
+        System.out.println("I am cylinder1 parameterized constructor");
+        this.height = h;
+    }
+
+    public double volume() {
+        return Math.PI * this.radius * this.radius * this.height;
+    }
+}
+
+public class Practice_set_10_Inheritance {
+    public static void main(String[] args) {
+        // Problem 1
+        // Circle objC = new Circle(12);
+        Cylinder1 obj = new Cylinder1(12, 4);
+    }
+}
