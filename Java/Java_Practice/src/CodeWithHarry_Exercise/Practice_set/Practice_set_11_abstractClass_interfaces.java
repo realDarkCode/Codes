c1fec0fd0bfdb6ebb66d082de4b2ca5a024bf461
Tanzil Islam <tanzil.ncpsc@gmail.com>
2022-03-14 21:28:40 +0600

    Practice_set/Practice_set_11_abstractClass_interfaces completed

--- Java/Java_Practice/src/CodeWithHarry_Exercise/Practice_set/Practice_set_11_abstractClass_interfaces.java
+++ Java/Java_Practice/src/CodeWithHarry_Exercise/Practice_set/Practice_set_11_abstractClass_interfaces.java
@@ -28,8 +28,10 @@ class FountainPen extends Pen {
 }
 
 // ? 3. Create a class monkey with jump ( ) and bite ( ) methods
-// Create a class human which inherits this monkey class and
-// implements basic animal interface with eat ( ) and sleep methods
+// ? Create a class human which inherits this monkey class and
+// ? implements basic animal interface with eat ( ) and sleep methods
+// ? 5. Demonstrate polymorphism using using monkey class from Q3
+
 interface Animal {
     void eat(String food);
 
@@ -56,6 +58,70 @@ class Human extends Monkey implements Animal {
     }
 }
 
+// ? 4. Create a class telephone with ( ) , lift ( ) and disconnected ( )
+// ? methods as abstract methods create another class smart telephone and
+// ? demonstrate polymorphism
+abstract class Telephone {
+    abstract void lift();
+
+    abstract void disconnected();
+}
+
+class SmartTelephone extends Telephone {
+    @Override
+    void lift() {
+        System.out.println("lifting up telephone");
+    }
+
+    @Override
+    void disconnected() {
+        System.out.println("the call has ended...");
+    }
+
+    void takePicture() {
+        System.out.println("flash flash, your ugly face is saved to gallery");
+    }
+
+    void playMusic() {
+        System.out.println("playing 'Silence - Before you exit'");
+    }
+}
+
+// ? 6. Create an interface TVremote and use it to inherit another interface
+// ? smart TVremote
+interface TVremote {
+
+    void turnOnTV();
+
+    void turnOffTV();
+
+    void changeChannel();
+}
+
+interface SmartTVRemote extends TVremote {
+    void sleep();
+
+    void recordProgram();
+}
+
+// ? 7. Create a class TV which implements TVremote interface from Q6
+class TV implements TVremote {
+    @Override
+    public void turnOnTV() {
+        System.out.println("Turning on the TV");
+    }
+
+    @Override
+    public void turnOffTV() {
+        System.out.println("Turning off the TV");
+    }
+
+    @Override
+    public void changeChannel() {
+        System.out.println("Changing the channel to BTV");
+    }
+}
+
 public class Practice_set_11_abstractClass_interfaces {
     public static void main(String[] args) {
         // // problem 1
@@ -71,5 +137,16 @@ public class Practice_set_11_abstractClass_interfaces {
         // theSavageBoy.bite();
         // theSavageBoy.jump();
         // theSavageBoy.sleep();
+        // // Problem 4
+        // Telephone nokia = new SmartTelephone();
+        // nokia.lift();
+        // nokia.disconnected();
+        // // problem 5
+        // Animal mashrafi = new Human();
+        // mashrafi.sleep();
+        // mashrafi.eat("Banana");
+        // // problem 6
+        // // problem 7
+
     }
 }
