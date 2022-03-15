15177722dd650e32df090f8b5ff4836f8f7c2e40
Tanzil Islam <tanzil.ncpsc@gmail.com>
2022-03-15 17:43:12 +0600

    practice set 12

--- Java/Java_Practice/src/CodeWithHarry_Exercise/Practice_set/Practice_set_12_customPackages_accessModifier/Problem3.java
+++ Java/Java_Practice/src/CodeWithHarry_Exercise/Practice_set/Practice_set_12_customPackages_accessModifier/Problem3.java
@@ -1,5 +1,5 @@
-// package CodeWithHarry_Exercise.Practice_set.Practice_set_12_customPackages_accessModifier;
-package folder.folderL1.folderL2;
+package CodeWithHarry_Exercise.Practice_set.Practice_set_12_customPackages_accessModifier;
+// package folder.folderL1.folderL2;
 // ? 3. Create a package in class with three package levels folder , folderL1 , folderL2 
 
 public class Problem3 {
