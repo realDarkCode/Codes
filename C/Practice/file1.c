a1cff61dea0d2b061ac63ef509b23bf4d58aff66
Tanzil Islam <tanzil.ncpsc@gmail.com>
2022-04-17 21:44:59 +0600

    temp test files

---
+++ C/Practice/file1.c
@@ -0,0 +1,11 @@
+#include "file2.c"
+#include<stdio.h>
+int a = 44; 
+
+int main(void) {
+
+  printf("num = %d\n", a);
+
+return 0;
+
+}
---
+++ C/Practice/test.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+typedef long long int LL;
+int main(void){
+	LL n = 50;
+	printf("everything seems allright\n");
+	return 0;
+}
